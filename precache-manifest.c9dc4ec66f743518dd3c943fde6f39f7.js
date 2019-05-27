34ac5be4dfe6dac75f9a304bb6ce07e47f4ab5fd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-27 14:32:47 +0000

    Deploy shufflify.ruud.ninja to github.com/tomasruud/shufflify.git:gh-pages

---
+++ precache-manifest.c9dc4ec66f743518dd3c943fde6f39f7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822e97f6ceb5a44d5619dc4e14ce6571",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e405e7b3955892d5cae2",
+    "url": "/static/js/2.148684d1.chunk.js"
+  },
+  {
+    "revision": "6544bdd07758f52b95b8",
+    "url": "/static/js/main.35087c38.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
